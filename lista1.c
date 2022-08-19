43365ee6391dc2edda076afd3f0a9a60106b99ea
juliaturazzi <juliaturazzi@gmail.com>
2022-08-18 21:19:07 -0300

    add lista1

---
+++ lista1.c
@@ -0,0 +1,26 @@
+/* 
+    Descrição: Este programa calcula a média de duas 
+    notas e informa se o aluno foi aprovado ou reprovado
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    float p1, p2, media;
+    p1 = p2 = media = 0;
+
+    printf("Nota da prova 1: ");
+    scanf("%f", &p1);
+
+    printf("Nota da prova 2: ");
+    scanf("%f", &p2);
+
+    media = (p1 + p2) / 2;
+
+    printf("Média: %f\n", media);
+
+    if (media >= 5) printf("Aprovado\n");
+    else printf("Reprovado\n");
+
+    return 0;
+}
